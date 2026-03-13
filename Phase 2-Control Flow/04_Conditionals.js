50d6141012fc810deac6c3c457a16824d934ee75
Parth-S-Mahale <parthmahale05@gmail.com>
2026-03-13 22:33:16 +0530

    02: control-flow

---
+++ Phase 2-Control Flow/04_Conditionals.js
@@ -0,0 +1,9 @@
+// Checking if a boolean value is true or false
+
+let isBoolean = false;
+
+if (isBoolean) {
+    console.log("This is a true statement");
+} else {
+    console.log("This is a false statement");
+}
---
+++ Phase 2-Control Flow/05_Conditionals.js
@@ -0,0 +1,11 @@
+// Checking if an array is empty or not
+
+let arr = [1, "string", true];
+
+console.log(`Length of array: ${arr.length}`);
+
+if (arr.length === 0) {
+    console.log("Array is empty");
+} else {
+    console.log("Array is NOT empty");
+}
